256adf5c9cf14aaa9d59b5f9fb2a40606fd0b2ce
Florencia Bazan <florenciacbazan@gmail.com>
2022-07-21 19:44:01 -0300

    Added option to calculate multiple values and creating objects

--- js/application.js
+++ js/application.js
@@ -3,7 +3,7 @@ function calculateTaxes() {
 
   let result;
 
-  // 21% IVA, 9% Impuesto PAIS, 35% Retroactivo a moneda extranjera
+  // 21% IVA, 9% Impuesto PAIS, 45% Retroactivo a moneda extranjera
 
   let taxes = 21 + 9 + 45;
   let taxes_percentage = (100 + taxes) / 100;
@@ -15,3 +15,30 @@ function calculateTaxes() {
     console.log('Invalid value, try again');
   }
 }
+
+function calculateMultipleTaxes() {
+  const value = prompt('Enter the values separated with comma: ').split(",");
+
+  let result;
+
+  // 21% IVA, 9% Impuesto PAIS, 45% Retroactivo a moneda extranjera
+
+  let taxes = 21 + 9 + 45;
+  let taxes_percentage = (100 + taxes) / 100;
+
+  if (value.length > 0) {
+    value.forEach(element => {
+      result = parseFloat(element * taxes_percentage).toFixed(2);
+      const history = new valuesHistory(result);
+      console.log(history);
+      console.log(`The amount with taxes is $${history.price} ARS`);
+    });
+  } else {
+    console.log('Invalid value, try again');
+  }
+}
+
+function valuesHistory(price) {
+  this.price = price;
+}
+
