49f89a52ae129e63541c4dba791189e169eec84b
Florencia Bazan <florenciacbazan@gmail.com>
2022-08-09 23:28:09 -0300

    Add library

--- js/application.js
+++ js/application.js
@@ -3,11 +3,11 @@ function darkMode(){
   SetTheme.classList.toggle("dark-mode")
   var theme;
   if(SetTheme.classList.contains("dark-mode")){
-      console.log("Dark mode");
-      theme = { theme: {color: "DARK"} };
+    console.log("Dark mode");
+    theme = { theme: {color: "DARK"} };
   }else{
-      console.log("Light mode");
-      theme = { theme: {color: "LIGHT"} };
+    console.log("Light mode");
+    theme = { theme: {color: "LIGHT"} };
   }
   localStorage.setItem("PageTheme", JSON.stringify(theme));
 }
@@ -16,9 +16,9 @@ const interval = setInterval(() => {
   let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
   console.log(GetTheme);
   if(GetTheme.theme.color === "DARK"){
-      document.body.classList = "dark-mode";
+    document.body.classList = "dark-mode";
   }else{
-      document.body.classList = "";
+    document.body.classList = "";
   }
 }, 5);
 setTimeout(() => clearInterval(interval), 10)
@@ -52,6 +52,15 @@ function calculateTaxes() {
       if (value >+ 0) {
         console.log(value);
         result = parseFloat(value * taxes_percentage).toFixed(2);
+        Swal.fire({
+          title: `The amount with taxes is $${result} ARS`,
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+        })
         console.log(`The amount with taxes is $${result} ARS`);
       } else {
         console.log('Invalid value, try again');
@@ -60,12 +69,17 @@ function calculateTaxes() {
   });
 }
 
-const MultipleTaxesInput = document.getElementById('calculateMultipleElement')
-MultipleTaxesInput.addEventListener('click', calculateMultipleTaxes);
+const multipleTaxesInput = document.getElementById('triggerInputMultiple')
+multipleTaxesInput.addEventListener('click', renderInputMultiple);
 
-function calculateMultipleTaxes() {
-  const values = prompt('Enter the values separated with comma: ').split(",");
-  
+function renderInputMultiple() {
+  input = document.getElementById('multipleCalculateInput')
+  input.removeAttribute("hidden");
+  calculateMultipleTaxes(input);
+}
+
+function calculateMultipleTaxes() {  
+  input = arguments[0];
   let result;
   
   // 21% IVA, 9% Impuesto PAIS, 45% Retroactivo a moneda extranjera
@@ -73,19 +87,38 @@ function calculateMultipleTaxes() {
   let taxes = 21 + 9 + 45;
   let taxes_percentage = (100 + taxes) / 100;
   
-  if (values.length > 0) {
-    values.forEach(value => {
-      result = parseFloat(value * taxes_percentage).toFixed(2);
-      const history = new valuesHistory(result);
-      console.log(history);
-      console.log(`The amount with taxes is $${history.price} ARS`);
-    });
-  } else {
-    console.log('Invalid value, try again');
-  }
+  input.addEventListener("keyup", function(event) {
+    if(event.key === 'Enter') {
+      var values = input.value.split(",");
+      console.log(values)
+      if (values.length >+ [0]) {
+        let prices = [];
+        values.forEach(value => {
+          result = parseFloat(value * taxes_percentage).toFixed(2);
+          const history = new valuesHistory(result);
+          console.log(history);
+          prices = prices.concat(history.price);
+          let html_prices = [];
+          prices.forEach(element => {
+            html_prices = html_prices.concat(`<p>The amount with taxes is $${element} ARS</p>`)
+          });
+          Swal.fire({
+            title: html_prices,
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+        });
+      } else {
+        console.log('Invalid value, try again');
+      }
+    }
+  });
 }
 
 function valuesHistory(price) {
   this.price = price;
 }
-
